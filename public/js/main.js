90b27586264a02c5f338080e8c83a7bb85f55540
Oriol Catasús <oriolcatasus@gmail.com>
2021-02-14 20:15:44 +0100

    Refactor carousel slides generation

--- public/js/main.js
+++ public/js/main.js
@@ -5,28 +5,17 @@ const numSlides = 6;
 $(async function() {
     const birds = await getAllBirds();
     const randNums = getNrandomNumbers(numSlides, birds.length);
-    const indicatorContainer = $('.carousel-indicators');
-    const indicator = $('<li data-bs-target="#carousel-birds">');
-    const itemContainer = $('.carousel-inner');
-    const item = $('<div class="carousel-item">');
-    const img = $('<img class="d-block w-100">');
-    item.append(img);
-    const caption = $('<div class="carousel-caption">');
-    item.append(caption);
-    const title = $('<h5>');
-    caption.append(title);
-    const subtitle = $('<i>');
-    caption.append($('<p>').append(subtitle));
     randNums.forEach((num, i) => {
         const bird = birds[num];
-        indicator.attr('data-bs-slide-to', i+1);
-        indicatorContainer.append(indicator.clone());
-        img.attr('src', bird.image);
-        img.attr('alt', bird.name);
-        title.text(bird.name);
-        subtitle.text(bird.scientificName);
-        itemContainer.append(item.clone());
+        $(`<li data-bs-target="#carousel-birds" data-bs-slide-to="${i+1}">`).appendTo('.carousel-indicators');
+        $(`<div class="carousel-item">
+            <img src="${bird.image}" alt="${bird.name}" class="d-block w-100">
+            <div class="carousel-caption">
+                <h5>${bird.name}</h5>
+                <p><i>${bird.scientificName}</i></p>
+            </div>
+        </div>`).appendTo('.carousel-inner');
     });
-    indicatorContainer.children().first().addClass('active');
-    itemContainer.children().first().addClass('active');
+    $('.carousel-indicators > li:first-child').addClass('active');
+    $('.carousel-inner > div:first-child').addClass('active');
 });
